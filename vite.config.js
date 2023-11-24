9466d8e363653b3931a20d4fd29c998a49e03505
louisecerezini <nutrilouiserodrigues@gmail.com>
2023-11-24 15:43:32 -0300

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pet-shop/"
+  base: "/pet-shop/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, '404.html') // Add this line if you're using a 404.html
+      }
+    }
+  }
 })
